5a6669d4b56ca7cac136ffca4fe25fa71ab1092d
Mamonova Olga <shamrilo91@mail.ru>
2022-10-04 01:02:52 +0300

    добавляет функции возврата рандомного целого числа и проверки длины строки

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// функция возвращает случайное целое число в заданном интервале включительно (источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || max <= min) {
+    return NaN;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomIntInclusive (5,567);
+
+// Функция для проверки максимальной длины строки
+
+function checkstringLength () {
+  const comment = document.querySelector('text__description');
+  const stringLength = comment.length;
+  const MIN_STRING_LENGTH = 20;
+  const MAX_STRING_LENGTH = 140;
+  return stringLength >= MIN_STRING_LENGTH && stringLength <= MAX_STRING_LENGTH;
+}
+
+checkstringLength();
