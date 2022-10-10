3ccdba46455461b3b048a84ba8df025ab1c5db38
Mamonova Olga <shamrilo91@mail.ru>
2022-10-11 00:13:47 +0300

    правит стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,30 +1,14 @@
 // функция возвращает случайное целое число в заданном интервале включительно (источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
 
-function getRandomIntInclusive (min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min < 0 || max < 0 || max <= min) {
-    return NaN;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  return (min < 0 || max < 0 || max <= min) ? NaN : Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-getRandomIntInclusive (5,567);
+getRandomIntInclusive(5,567);
 
 // Функция для проверки максимальной длины строки
 
-function checkstringLength (stringLength, minStringLength, maxStringLength) {
-  return stringLength >= minStringLength && stringLength <= maxStringLength;
-}
-
-checkstringLength(36, 20, 140);
-
-// function checkstringLength () {
-//   const comment = document.querySelector('text__description');
-//   const stringLength = comment.length;
-//   const MIN_STRING_LENGTH = 20;
-//   const MAX_STRING_LENGTH = 140;
-//   return stringLength >= MIN_STRING_LENGTH && stringLength <= MAX_STRING_LENGTH;
-// }
-
-// checkstringLength();
+const checkStringLength = (stringLength, maxStringLength) => stringLength <= maxStringLength;
+checkStringLength(36, 140);
