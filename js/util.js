547d712409be38780e9871876e90ee8c93b2a7dc
Mamonova Olga <shamrilo91@mail.ru>
2022-10-13 11:29:20 +0300

    добавляет модули

---
+++ js/util.js
@@ -0,0 +1,21 @@
+// функция возвращает случайное целое число в заданном интервале включительно (источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || max <= min) {
+    return NaN;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Функция возращает случайный элемент в указанном массиве
+
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+// Функция для проверки максимальной длины строки
+
+const checkStringLength = (stringLength, maxStringLength) => stringLength <= maxStringLength;
+checkStringLength(36, 140);
+
+export {getRandomIntInclusive, getRandomArrayElement};
