5989bc1aba03e8f19500c5dd5dfdc2c566ed39bb
Mamonova Olga <shamrilo91@mail.ru>
2022-10-28 14:41:29 +0300

    добавляет валидацию формы

--- js/img-edit-form.js
+++ js/img-edit-form.js
@@ -2,33 +2,49 @@ import {isEscapeKey} from './util.js';
 
 const uploadFileInput = document.querySelector('#upload-file');
 const body = document.querySelector('body');
-const imgEditForm = document.querySelector('.img-upload__overlay');
+const imgEditor = document.querySelector('.img-upload__overlay');
 const uploadCloseButton = document.querySelector('#upload-cancel');
 const commentText = document.querySelector('.text__description');
+const imgEditForm = document.querySelector('.img-upload__form');
 
-function onImgEditFormEscKeydown(evt) {
+function onImgEditorEscKeydown(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    imgEditForm.classList.add('hidden');
+    imgEditor.classList.add('hidden');
     body.classList.remove('modal-open');
   }
 }
 
-function openImgEditForm() {
-  imgEditForm.classList.remove('hidden');
+function openImgEditor() {
+  imgEditor.classList.remove('hidden');
   body.classList.add('modal-open');
-  document.addEventListener('keydown', onImgEditFormEscKeydown);
+  document.addEventListener('keydown', onImgEditorEscKeydown);
 }
 
-function closeImgEditForm() {
-  imgEditForm.classList.add('hidden');
+function closeImgEditor() {
+  imgEditor.classList.add('hidden');
   body.classList.remove('modal-open');
   // document.querySelector('.scale__control--value').value = 100; как масштаб задать 100% при закрытии?
   document.querySelector('#effect-none').checked = true;
   commentText.value = '';
   uploadFileInput.value = '';
-  document.removeEventListener('keydown', onImgEditFormEscKeydown);
+  document.removeEventListener('keydown', onImgEditorEscKeydown);
 }
 
-uploadFileInput.addEventListener('change', openImgEditForm);
-uploadCloseButton.addEventListener('click', closeImgEditForm);
+uploadFileInput.addEventListener('change', openImgEditor);
+uploadCloseButton.addEventListener('click', closeImgEditor);
+
+// const validateComment = new Pristine(imgEditForm, {
+//   classTo: 'text__label',
+//   errorTextParent: 'text__label',
+//   errorTextClass: 'text__error-text',
+// });
+
+// imgEditForm.addEventListener('submit', (evt) => {
+//   const isValid = validateComment.validate();
+//   if (!isValid) {
+//     evt.preventDefault();
+//   }
+// });
+
+// validateComment();
